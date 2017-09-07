cf317d21afb5066632966980a26579f08c695b07
Brock Ellis <brock@sharproot.com>
2017-09-07 12:32:53 -0500

    I'm watching you....

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,32 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/map';
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  public productivity:any; // should be an Interface 
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, public http: Http) {
 
+  }
+
+  ionViewDidLoad(){
+    this.getProductivity();
+  }
+
+  getProductivity(){
+    this.http.get('https://api.noahkoch.com/v1/activities/productivity').map(res => res.json()).subscribe(data => {
+      this.productivity = data;
+    });
+  }
+
+  refresh(){
+    this.getProductivity();
   }
 
 }
